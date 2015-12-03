ec7b8b060b40ff49499de38f4deba268a6ccc9e2
Alexandra Carter <alexandra.carternz@gmail.com>
2015-12-03 14:52:56 +1300

    optimized images

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,12 @@ $(document).ready(function() {
       // JQUERY NAV TOGGLE
       $('#icon').bind('click',function(event){
           $('.mainnav').slideToggle();
-
-				$(".scroll").click(function(event){		
+          });
+		$(".scroll").click(function(event){		
 				event.preventDefault();
 				$('html,body').animate({
 					scrollTop:$(this.hash).offset().top}, 
-					500
+					1000
 					);
 				});
-
-		});
   });
